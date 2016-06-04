fa78f14e945a01add79fae4737fdbb93920ace75
Emerson Loustau <e@thesecretaddress.com>
2016-06-04 15:49:41 -0700

    post works

---
+++ app.js
@@ -0,0 +1,41 @@
+var smashingMarkup = require('./index');
+var express = require('express');
+var bodyParser = require("body-parser");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var app = express();
+
+app.use(express.static("./public"));
+// app.get('/', function(req, res){
+//   // res.sendfile('./public/index.html');
+// });
+
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+
+// });
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.listen(3000);
+app.post("/convert", function(req, res){
+    
+    smashingMarkup.mdToHtml(req.body.markdown)
+        .then( function(html){
+            responseData = { markup : html};
+            res.json(responseData);
+        });
+});
+console.log("Express app running on port 3000");
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 var pdc = require('pdc');
 var path = require('path');
 var fs = require('fs');
+var Q = require('q');
 
 // optional, if pandoc is not in PATH
 pdc.path = path.resolve(__dirname +'/pandoc/1.15.0.6/bin/pandoc');
 
-var inputFilePath = process.argv[2];
-console.log(inputFilePath);
-var inputFile = fs.readFileSync(inputFilePath, 'utf8');
+// var inputFilePath = process.argv[2];
+// console.log(inputFilePath);
+// var inputFile = fs.readFileSync(inputFilePath, 'utf8');
 
-/*
-src is a string containing the entire source text, that shall be converted.
-from is a string containing the type of the source text (e.g. 'markdown').
-to is a string containing the type of the destination text (e.g. 'html').
-args [optional] is an array with additional command line flags (e.g. [ '-v' ] for pandoc's version).
-opts [optional] is an object with additional options for the process. See the Node.js docs.
-callback is a function that is called after parsing. It takes two arguments (err, result), where err is an error or null and result is a string containing the converted text.
- */
-// pdc(src, from, to, [args,] [opts,] callback);
-pdc(inputFile, 'markdown', 'html', ['--template=smashingtemplate'], function(err, result) {
-    if (err)
-        throw err;
-    result = replaceImgsWithFigures(result);
-    console.log(result);
-    fs.writeFileSync(inputFilePath+'.html', result, 'utf8');
-});
+module.exports.mdToHtml = function (input, callback) {
 
+    var deferred = Q.defer();
+
+    /*
+    src is a string containing the entire source text, that shall be converted.
+    from is a string containing the type of the source text (e.g. 'markdown').
+    to is a string containing the type of the destination text (e.g. 'html').
+    args [optional] is an array with additional command line flags (e.g. [ '-v' ] for pandoc's version).
+    opts [optional] is an object with additional options for the process. See the Node.js docs.
+    callback is a function that is called after parsing. It takes two arguments (err, result), where err is an error or null and result is a string containing the converted text.
+     */
+    // pdc(src, from, to, [args,] [opts,] callback);
+    pdc(input, 'markdown', 'html', ['--template=smashingtemplate'], function(err, result) {
+        if (err) {
+            deferred.reject(err)
+        }
+        result = replaceImgsWithFigures(result);
+        deferred.promise.nodeify(callback);
+        deferred.resolve(result);
+    });
+
+    return deferred.promise;
+
+}
 
 
 function replaceImgsWithFigures(inputString) {
